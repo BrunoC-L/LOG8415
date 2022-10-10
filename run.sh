6a5d7d066dca7fa07e176fc5f68f768c88aeebe8
BrunoC-L <bruno.bcl@sympatico.ca>
2022-10-10 01:12:54 -0400

    added run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+python3 -m pip install --upgrade pip
+pip3 install boto3
+
+# aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].Name'
+# test=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].Name')
+# echo $test
+
+ECSImageId=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
+aws ec2 run-instances --image-id $ECSImageId --instance-type t2.micro --key-name vockey
