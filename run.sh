960a733f6cc58e5bbe9b6ef20567897d49f76e7a
Bruno Curzi-Laliberte <43907476+BrunoC-L@users.noreply.github.com>
2022-10-11 16:53:21 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,26 @@
 python3 -m pip install --upgrade pip
 pip3 install boto3
 
-# aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].Name'
-# test=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].Name')
+echo "
+python3 -m pip install --upgrade pip
+pip install flask
+
+echo \"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def my_app():
+    return 'First Flask Application!'
+\" > app.py
+
+python -m flask --app app run --port 8080
+" > deployflask.sh
+
 
 ECSImageId=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
 # silently run instances
-aws ec2 run-instances --image-id $ECSImageId --count 4 --instance-type t2.micro --key-name vockey > /dev/null 2>&1
+aws ec2 run-instances --image-id $ECSImageId --count 1 --instance-type t2.micro --key-name vockey --user-data file://deployflash.sh> /dev/null 2>&1
 
 # terminate all instances
 aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --query "Reservations[].Instances[].[InstanceId]" --output text)
