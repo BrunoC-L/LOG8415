44fd6f041845dcb6ee0c15156239070d3c9b21d6
Bruno Curzi-Laliberte <43907476+BrunoC-L@users.noreply.github.com>
2022-10-11 16:24:11 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ pip3 install boto3
 
 # aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].Name'
 # test=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].Name')
-# echo $test
 
 ECSImageId=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
-aws ec2 run-instances --image-id $ECSImageId --instance-type t2.micro --key-name vockey
+# silently run instances
+aws ec2 run-instances --image-id $ECSImageId --count 4 --instance-type t2.micro --key-name vockey > /dev/null 2>&1
 
 # terminate all instances
-# aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --query "Reservations[].Instances[].[InstanceId]" --output text)
+aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --query "Reservations[].Instances[].[InstanceId]" --output text)
