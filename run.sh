7c01f374072d3431383fbc14fd44df982ea3fe86
Bruno Curzi-Laliberte <43907476+BrunoC-L@users.noreply.github.com>
2022-10-11 17:38:06 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ printf "python3 -m pip install --upgrade pip \npip install flask \nprintf \"from
 
 ECSImageId=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
 
+SecurityGroup=$(aws ec2 create-security-group --description "Flask Group" --group-name flask-group --output-text)
+aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tcp --port 22
 aws ec2 run-instances --image-id $ECSImageId --count 1 --instance-type t2.micro --key-name vockey --user-data file://deployflask.sh --query "Instances[].[InstanceId]" --output text
 
 # terminate all instances
