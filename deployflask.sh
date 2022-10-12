ece003810b22e471290ff502920f543ec0b1453a
Mirado Rabenasolo <mirado.rabenasolo@telecom-paris.fr>
2022-10-12 20:51:13 +0100

    Flask application avec redirection de port par nginx, il reste à trouver comment spécifier l'adresse IP de l'instance dans le code

--- deployflask.sh
+++ deployflask.sh
@@ -2,16 +2,30 @@
 apt-get update
 apt-get install -y python3
 apt-get install -y python3-pip
-apt-get install -y python3-venv
+apt-get install -y nginx
+apt-get install -y gunicorn3 
+
 mkdir flask_application && cd flask_application
-python3 -m venv venv
-source venv/bin/activate
 pip install Flask
 echo "from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
 def my_app():
-    return 'First Flask Application'" > my_app.py
-export flask_application=my_app.py
-flask run --host=0.0.0.0 --port=80
+    return 'First Flask Application'
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=8080)" > my_app.py
+
+cd etc/nginx/
+cd sites-enabled/
+echo "server {
+    listen 80;
+    server_name instance_public_ip;
+
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+    }
+}" > flaskapp
+service nginx restart
+cd ~
+cd flask_application
+gunicorn3 my_app:my_app
--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ ECSImageId=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values
 SecurityGroup=$(aws ec2 create-security-group --description "Flask Group" --group-name flask-group --output text)
 aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tcp --port 80 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tcp --port 8080 --cidr 0.0.0.0/0
 
 aws ec2 run-instances --image-id $ECSImageId --count 1 --instance-type t2.micro --security-group-ids $SecurityGroup --key-name vockey --user-data file://deployflask.sh --query "Instances[].[InstanceId]" --output text
 
