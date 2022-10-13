3fdc3e6a749aebe740040835d2e1ae9d897f3003
Mirado Rabenasolo <mirado.rabenasolo@telecom-paris.fr>
2022-10-13 06:24:30 +0100

    spaw

--- run.sh
+++ run.sh
@@ -4,12 +4,13 @@ pip3 install boto3
 
 # printf "python3 -m pip install --upgrade pip \npip install flask \nprintf \"from flask import Flask\\napp = Flask(__name__)\\n@app.route('/')\\ndef my_app():\\n  return 'app'\" > app.py\napp.py flask run --port 8080\n" > deployflask.sh
 
-printf '#!%s'"/bin/bash \napt-get update \napt-get install -y python3 \napt-get install -y python3-pip \napt-get install -y nginx \napt-get install -y gunicorn3 \nmkdir flask_application \ncd flask_application \npip install Flask \necho \"from flask import Flask \napp = Flask(__name__) \n@app.route('/') \ndef my_app(): \n\treturn 'First Flask Application' \nif __name__=='__main__': \n\tapp.run(host='0.0.0.0', port=8080)\" > my_app.py \ncd /etc/nginx/ \ncd sites-enabled/ \necho \"server { \nlisten 80; \n\tserver_name \">flaskapp \ncurl http://169.254.169.254/latest/meta-data/public-ipv4 >> flaskapp \necho \"; \n\nlocation / { \n\tproxy_pass http://127.0.0.1:8000; \n\t} \n}\" > flaskapp \nservice nginx restart \ncd ~ \ngunicorn3 my_app:my_app" > deployFlask.sh
+printf '#!%s'"/bin/bash \napt-get update \napt-get install -y python3 \napt-get install -y python3-pip \napt-get install -y nginx \napt-get install -y gunicorn3 \nmkdir flask_application \ncd flask_application \npip install Flask \necho \"from flask import Flask \napp = Flask(__name__) \n@app.route('/') \ndef my_app(): \n\treturn 'First Flask Application' \nif __name__=='__main__': \n\tapp.run(host='0.0.0.0', port=8080)\" > my_app.py \ncd /etc/nginx/ \ncd sites-enabled/ \necho \"server { \nlisten 80; \n\tserver_name \">flaskapp \ncurl http://169.254.169.254/latest/meta-data/public-ipv4 >> flaskapp \necho \"; \n\nlocation / { \n\tproxy_pass http://127.0.0.1:8000; \n\t} \n}\" >> flaskapp \nservice nginx restart \ncd ~ \ngunicorn3 my_app:my_app" > deployFlask.sh
 
 # ECSImageId=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
 
 ECSImageId=ami-09a41e26df464c548
-SecurityGroup=$(aws ec2 create-security-group --description "Flask Group" --group-name flask-group --output text)
+#SecurityGroup=$(aws ec2 create-security-group --description "Flask Group" --group-name flask-group --output text)
+SecurityGroup=sg-0666cf92245a33566
 aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tcp --port 80 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tcp --port 8080 --cidr 0.0.0.0/0
