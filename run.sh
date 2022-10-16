840c9f3c7304c05ae524f85a25f2cfe7c421a111
Bruno Curzi-Laliberte <43907476+BrunoC-L@users.noreply.github.com>
2022-10-16 00:44:37 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
-# open this as raw copy url and run `curl <url> > run.sh && sh run.sh`
-python3 -m pip install --upgrade pip
-pip3 install boto3
+# Open this as raw copy url and run `curl <url> > run.sh && sh run.sh`
 
-# printf "python3 -m pip install --upgrade pip \npip install flask \nprintf \"from flask import Flask\\napp = Flask(__name__)\\n@app.route('/')\\ndef my_app():\\n  return 'app'\" > app.py\napp.py flask run --port 8080\n" > deployflask.sh
-
-# printf '#!%s'"/bin/bash \napt-get update \napt-get install -y python3 \napt-get install -y python3-pip \napt-get install -y nginx \napt-get install -y gunicorn3 \nmkdir flask_application \ncd flask_application \npip install Flask \necho \"from flask import Flask \napp = Flask(__name__) \n@app.route('/') \ndef my_app(): \n\treturn 'First Flask Application' \nif __name__=='__main__': \n\tapp.run(host='0.0.0.0', port=8080)\" > my_app.py \ncd /etc/nginx/ \ncd sites-enabled/ \necho \"server { \nlisten 80; \n\tserver_name \">flaskapp \ncurl http://169.254.169.254/latest/meta-data/public-ipv4 >> flaskapp \necho \"; \n\nlocation / { \n\tproxy_pass http://127.0.0.1:8000; \n\t} \n}\" >> flaskapp \nservice nginx restart \ncd ~ \ncd /flask_application \ngunicorn3 my_app:my_app" > deployFlask.sh
-
+# Write script to run on the instances as `deployFlask.sh`
 echo '#!/bin/bash'"
 apt-get update
 apt-get install -y python3
@@ -70,23 +65,30 @@ server {
 
 systemctl restart nginx" > deployFlask.sh
 
+python3 -m pip install --upgrade pip
+pip3 install boto3
+
 # ECSImageId=$(aws ec2 describe-images --owners amazon --filters "Name=name,Values=amzn2-ami-ecs*" --query 'sort_by(Images, &CreationDate)[].Name' --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
 ECSImageId=ami-09a41e26df464c548
 
 DefaultSecurityGroup=$(aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --filters Name=group-name,Values=default --output text)
-OldGroups=$(aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --output text)
-# terminate all instances in case of dependency to sg (which means we cant delete sg)
+
 OldInstances=$(aws ec2 describe-instances --query "Reservations[].Instances[].[InstanceId]" --output text)
-for instance in $OldInstances
-do
-aws ec2 modify-instance-attribute --instance-id $instance --groups $DefaultSecurityGroup
-done
-aws ec2 terminate-instances --instance-ids $OldInstances
+if [ $OldInstances != "" ]
+    for instance in $OldInstances
+    do
+        # remove dependency to sg (which means we cant delete sg), this has to be done while the instance is running or stopped (not terminating)
+        aws ec2 modify-instance-attribute --instance-id $instance --groups $DefaultSecurityGroup
+    done
+    aws ec2 terminate-instances --instance-ids $OldInstances
+fi
+
+OldGroups=$(aws ec2 describe-security-groups --query "SecurityGroups[].GroupId" --output text)
 for group in $OldGroups
 do
-if [ "$group" != "$DefaultSecurityGroup" ]; then
-    aws ec2 delete-security-group --group-id $group
-fi
+    if [ "$group" != "$DefaultSecurityGroup" ]; then
+        aws ec2 delete-security-group --group-id $group
+    fi
 done
 
 SecurityGroup=$(aws ec2 create-security-group --description "Flask Group" --group-name flask-group --output text)
@@ -96,48 +98,39 @@ aws ec2 authorize-security-group-ingress --group-id $SecurityGroup --protocol tc
 
 aws ec2 run-instances --image-id $ECSImageId --count 1 --instance-type t2.micro --security-group-ids $SecurityGroup --key-name vockey --user-data file://deployFlask.sh --query "Instances[].[InstanceId]" --output text
 
-#get vpc id 
+# get vpc id 
 VpcId=$(aws ec2 describe-vpcs --query 'Vpcs'[0].VpcId --output text)
 
-## create target group
-aws elbv2 create-target-group --name cluster1 --protocol HTTP --port 80 --target-type instance --vpc-id $VpcId
-aws elbv2 create-target-group --name cluster2 --protocol HTTP --port 80 --target-type instance --vpc-id $VpcId
+# create target groups
+Cluster1Name=cluster1
+Cluster2Name=cluster2
+TargetGroupArn1=$(aws elbv2 create-target-group --name $cluster1Name --protocol HTTP --port 80 --target-type instance --vpc-id $VpcId --query 'TargetGroups'[0].TargetGroupArn --output text)
+TargetGroupArn2=$(aws elbv2 create-target-group --name #cluster2Name --protocol HTTP --port 80 --target-type instance --vpc-id $VpcId --query 'TargetGroups'[0].TargetGroupArn --output text)
 
-#stocker les TargetGroupArn
-TargetGroupArn1=$(aws elbv2 describe-target-groups --query 'TargetGroups'[0].TargetGroupArn --output text)
-TargetGroupArn2=$(aws elbv2 describe-target-groups --query 'TargetGroups'[1].TargetGroupArn --output text)
-
-## add instances to target-group
-#TO DO : obtenir les instance id 
+# add instances to target-group
+# TODO : obtenir les instance id 
 
 #cluster 1
-#aws elbv2 register-targets --target-group-arn TargetGroupArn1  -targets Id=#TO DO instance ID i-0abcdef1234567890 Id=i-1234567890abcdef0
+#aws elbv2 register-targets --target-group-arn TargetGroupArn1 -targets Id=TODO instance ID i-0abcdef1234567890 Id=i-1234567890abcdef0
 #cluster 2
-#aws elbv2 register-targets --target-group-arn TargetGroupArn2  -targets Id=TO DO i-0abcdef1234567890 Id=i-1234567890abcdef0
+#aws elbv2 register-targets --target-group-arn TargetGroupArn2 -targets Id=TODO i-0abcdef1234567890 Id=i-1234567890abcdef0
 
 
-## create loadbalancer 
-aws elbv2 create-load-balancer --name my-load-balancer  --subnets subnet-04f197a80f791c8ed  subnet-0e39e8d6bc1a91173 
+# create loadbalancer
+# TODO subnet ids
+LoadBalancerArn=$(aws elbv2 create-load-balancer --name my-load-balancer --subnets subnet-04f197a80f791c8ed subnet-0e39e8d6bc1a91173 --query 'LoadBalancers'[0].LoadBalancerArn --output text)
 
-#stocker LoadBalancerArn
-LoadBalancerArn=$(aws elbv2 describe-load-balancers --query 'LoadBalancers'[0].LoadBalancerArn --output text)
+# setup listener rules of the loadbalancer 
+ListenerArn1=$(aws elbv2 create-listener --load-balancer-arn $LoadBalancerArn --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$TargetGroupArn1 --query 'Listeners'[0].ListenerArn --output text)
+
+aws elbv2 create-rule --listener-arn $ListenerArn1 --priority 10 --conditions Field=path-pattern,Values='/$Cluster1Name' --actions Type=forward,TargetGroupArn=$TargetGroupArn1
+aws elbv2 create-rule --listener-arn $ListenerArn1 --priority 9 --conditions Field=path-pattern,Values='/$Cluster2Name' --actions Type=forward,TargetGroupArn=$TargetGroupArn2
 
 
-##setup listener rules of the loadbalancer 
-aws elbv2 create-listener --load-balancer-arn $LoadBalancerArn --protocol HTTP --port 80  --default-actions Type=forward,TargetGroupArn=$TargetGroupArn1
-
-#listener arn
-ListenerArn1=$(aws elbv2 describe-listeners --load-balancer-arn $LoadBalancerArn --query 'Listeners'[0].ListenerArn --output text)
-
-aws elbv2 create-rule --listener-arn $ListenerArn1 --priority 10 --conditions Field=path-pattern,Values='/cluster1' --actions Type=forward,TargetGroupArn=$TargetGroupArn1
-aws elbv2 create-rule --listener-arn $ListenerArn1 --priority 9 --conditions Field=path-pattern,Values='/cluster2' --actions Type=forward,TargetGroupArn=$TargetGroupArn2
-
-   
-
-#delete loadbalancer 
+# delete loadbalancer 
 aws elbv2 delete-load-balancer --load-balancer-arn $LoadBalancerArn
 
-#get targetgroup arn and delete the two targetgroups one by one
+# get target group arn and delete the two target groups one by one
 aws elbv2 delete-target-group --target-group-arn $TargetGroupArn1
 aws elbv2 delete-target-group --target-group-arn $TargetGroupArn2
 
