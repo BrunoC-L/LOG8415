3b6f3f854d44be892fbcf1a14b29cfd6cd03a8ce
Mirado Rabenasolo <mirado.rabenasolo@telecom-paris.fr>
2022-10-29 00:36:22 +0100

    spark

--- tp2/installSpark.sh
+++ tp2/installSpark.sh
@@ -9,7 +9,7 @@ wget https://dlcdn.apache.org/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz
 
 tar -xvzf spark-3.3.1-bin-hadoop3.tgz
 
-mv spark-3.3.1-bin-hadoop3 /opt/spark
+sudo mv spark-3.3.1-bin-hadoop3 /opt/spark
 
 echo "SPARK_HOME=/opt/spark
 PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
