b8cec46ad0a6370040996795551d01dde08b9f1c
Mirado Rabenasolo <mirado.rabenasolo@telecom-paris.fr>
2022-10-28 23:19:12 +0100

    Slight changes in wordCount program, nothing done in friendRecommendation

--- tp2/wordCountExecution.sh
+++ tp2/wordCountExecution.sh
@@ -7,17 +7,17 @@ curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/WordCount.java 
 hadoop com.sun.tools.javac.Main WordCount.java
 jar cf wc.jar WordCount*.class
 
-mkdir input
-cd input
-
 # Downloading input data
 curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/pg4300.txt >./pg4300.txt
+
+
+hdfs dfs -mkdir input
+hdfs dfs -copyFromLocal pg4300.txt input
+
 
 # Test files 
 #echo 'Hello World Bye World'>> file01
 #echo 'Hello Hadoop Goodbye Hadoop'>> file02
-
-cd ..
 
 # Running the application
 hadoop jar wc.jar WordCount input output
