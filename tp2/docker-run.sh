57104bb79197c54db554de3399fcf9b457b52a0b
Mirado Rabenasolo <mirado.rabenasolo@telecom-paris.fr>
2022-10-31 23:29:29 +0000

    Added Hadoop vs Spark

--- tp2/docker-run.sh
+++ tp2/docker-run.sh
@@ -3,6 +3,8 @@
 curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/soc-LiveJournal1Adj.txt > soc-LiveJournal1Adj.txt
 curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/network_problem.py > network_problem.py
 curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/hadoopVsLinux.sh > hadoopVsLinux.sh
+curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/hadoopVsSpark.sh > hadoopVsSpark.sh
 
 bash hadoopVsLinux.sh
+bash hadoopVsSpark.sh
 python3 network_problem.py
