082e2e30329a0018cbec8fc85e3bfd15bdece93e
Bruno Curzi-Laliberte <43907476+BrunoC-L@users.noreply.github.com>
2022-10-31 17:53:14 -0400

    Update hadoopVsLinux.sh

--- tp2/hadoopVsLinux.sh
+++ tp2/hadoopVsLinux.sh
@@ -15,10 +15,12 @@ curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/pg4300.txt >/us
 #echo 'Hello Hadoop Goodbye Hadoop'>> file02
 
 # Running the application on hadoop
-time hadoop jar wc.jar WordCount input output
+echo "HADOOP " >> /usr/local/hadoop-3.3.4/linux_result.txt
+time hadoop jar wc.jar WordCount input output  >> /usr/local/hadoop-3.3.4/linux_result.txt
 # Results
 # hadoop fs -cat ./output/part-r-00000
 
+echo "LINUX " >> /usr/local/hadoop-3.3.4/linux_result.txt
 #running on linux 
 time cat input/pg4300.txt | tr ' ' '\n' | sort | uniq -c >> /usr/local/hadoop-3.3.4/linux_result.txt
 
