f10029c8cea200015673f36f46c8695d9fb12882
BrunoC-L <bruno.bcl@sympatico.ca>
2022-10-30 23:41:54 -0400

    fixed a typo, added some info in the code as comments and outputting 2 intermediate result files to understand the process better

--- tp2/network_problem.py
+++ tp2/network_problem.py
@@ -17,21 +17,6 @@ def parse_line(line):
     else :
         return (user, [])
 
-# # parse a line of the txt file to an array like (0,[1,2,3,4,5,...])
-# def parse_line(line):
-#     array = line.split()
-#     # first number is the user
-#     user = int(array[0])
-#     # no other number means there are no friends
-#     if len(array) == 1 : 
-#         friends = []
-#     # all numbers after user are friends' user
-#     else :
-#         # convert to list of int
-#         friends = list(map(int, array[1:][0].split(',')))
-
-#     return (user, friends)
-
 # make connection between user and all friend, and between all his friend and specify if there are friend or not with 0 or 1 flag
 # output : [((0,1),0), ((0,2),1), ....] --> users 0 and 1 are friends and users 0 and 2 have a mutual friend
 def create_friend_connection(user, friends):
@@ -95,63 +80,54 @@ def recommendation_to_sorted_truncated(recs):
     # recommendations are sorted from best to worst
     recommendations = []
     for rec in recs:
-        if len(recommendations) == 0 or aBetterThanB(rec, recs[-1]):
+        if len(recommendations) == 0 or aBetterThanB(rec, recommendations[-1]):
             insert(recommendations, rec)
     return [user_id for user_id, n_friends in recommendations]
+
+def save(obj, loc):
+    try:
+        # remove spark output location in case it has been used
+        for f in listdir(loc):
+            remove(loc + '/' + f)
+        removedirs(loc)
+    except:
+        # would happen if the location didn't exist, which is fine
+        pass
+
+    obj.saveAsTextFile(path=loc)
+
 
 def spark_run():
     # TODO remove refs to SHORT for release
     SHORT=True
 
-    # Initialize spark configuration and context
     conf = SparkConf().set("spark.executor.cores", 4).set("spark.executor.instances", 4)
     sc = SparkContext(conf=conf)
 
-    # Read from text file, split each line into "words" by any whitespace (i.e. empty parameters to string.split())
     fileLoc = 'soc-LiveJournal1Adj.txt'
     if SHORT:
         fileLoc = 'soc-LiveJournal1Adj-short.txt'
-    print(fileLoc)
     lines = sc.textFile(fileLoc)
 
-    # Map each line to the form: (user_id, [friend_id_0, friend_id_1, ...])
-    # Map each "friend ownership" to multiple instances of ((user_id, friend_id), VALUE).
-    # VALUE = 0 indicates that user_id and friend_id are already friends.
-    # VALUE = 1 indicates that user_id and friend_id are not friends.
-
-    def our_mapper(line):
-        return create_friend_connection(*parse_line(line))
+    # friend edges are of shapes ((user_id1, user_id2), 0 or 1) with 0 meaning friends, 1 meaning mutual friend
+    friend_edges = lines.flatMap(lambda line: create_friend_connection(*parse_line(line))).cache()
+    save(friend_edges, "./result-network-problem-friends")
     
-    friend_edges = lines.flatMap(our_mapper)
-    friend_edges.cache()
-    
-    # Filter all pairs of users that are already friends, then sum all the "1" values to get their mutual friend count.
+    # Select pairs of indirect friends then sum all the values to get their mutual friend count
+    # mutual friend counts are of shapes ((user_id1, user_id2), count)
     mutual_friend_counts = friend_edges.groupByKey() \
-        .filter(lambda edge: 0 not in edge[1]) \
-        .map(lambda edge: (edge[0], sum(edge[1])))
-    mutual_friend_counts.cache()
+        .filter(lambda edge: edge[1] != VALUE_DIRECT_FRIEND) \
+        .map(lambda edge: (edge[0], sum(edge[1]))).cache()
+    save(mutual_friend_counts, "./result-network-problem-counts")
 
     # Create the recommendation objects, group them by key, then sort and truncate the recommendations to the 10 most highly recommended.
+    # recommendations are of shapes (user_id1, [recs...]) where recs is a length 0..10 array
     recommendations = mutual_friend_counts \
         .flatMap(lambda conn_count: mutual_friend_count_to_recommendation(*conn_count)) \
         .groupByKey() \
         .map(lambda edge: (edge[0], recommendation_to_sorted_truncated(list(edge[1]))))
 
-    outLoc = "./result-network-problem"
-    if SHORT:
-        outLoc += '-short'
-    print(outLoc)
-
-    try:
-        # remove spark output location in case it has been used
-        for f in listdir(outLoc):
-            remove(outLoc + '/' + f)
-        removedirs(outLoc)
-    except:
-        # would happen if the location didn't exist, which is fine
-        pass
-
-    recommendations.saveAsTextFile(path=outLoc)
+    save(recommendations, "./result-network-problem-recommendations")
     sc.stop()
 
 spark_run()
