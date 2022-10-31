3e820471a644f19462e2e83c41207d152593e6a4
aicha04 <miloudi.ai04@gmai.com>
2022-10-31 15:10:43 -0400

    added hadoop vs linux to docker-run.sh

--- tp2/docker-run.sh
+++ tp2/docker-run.sh
@@ -2,5 +2,6 @@
 
 curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/soc-LiveJournal1Adj.txt > soc-LiveJournal1Adj.txt
 curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/network_problem.py > network_problem.py
-
+curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/hadoopVsLinux.sh > /hadoopVsLinux.sh
+./hadoopVsLinux.sh
 python3 network_problem.py
--- tp2/hadoopVsLinux.sh
+++ tp2/hadoopVsLinux.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 # With hadoop 
-cd /usr/local/hadoop-3.3.4
 # Downloading wordcount java file
-curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/WordCount.java >./WordCount.java
+curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/WordCount.java >/usr/local/hadoop-3.3.4/WordCount.java
 
 # Creating wordcount jar file
 hadoop com.sun.tools.javac.Main WordCount.java
 jar cf wc.jar WordCount*.class
-mkdir input
+mkdir /usr/local/hadoop-3.3.4/input
 # Downloading input data
-curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/pg4300.txt >./input/pg4300.txt
-
-
-# hdfs dfs -mkdir input
-# hdfs dfs -copyFromLocal pg4300.txt input
-
+curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/pg4300.txt >/usr/local/hadoop-3.3.4/input/pg4300.txt
 
 # Test files 
 #echo 'Hello World Bye World'>> file01
@@ -23,7 +17,9 @@ curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/tp2/pg4300.txt >./i
 # Running the application on hadoop
 time hadoop jar wc.jar WordCount input output
 # Results
-hadoop fs -cat ./output/part-r-00000
+# hadoop fs -cat ./output/part-r-00000
 
 #running on linux 
-time cat input/pg4300.txt | tr ' ' '\n' | sort | uniq -c >> linux_result.txt
+time cat input/pg4300.txt | tr ' ' '\n' | sort | uniq -c >> /usr/local/hadoop-3.3.4/linux_result.txt
+
+# cat /usr/local/hadoop-3.3.4/linux_result.txt
