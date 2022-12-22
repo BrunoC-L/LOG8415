6d98016733cc1a98dfc6436a753869e9d7a12eae
BrunoC-L <bruno.bcl@sympatico.ca>
2022-12-22 03:26:34 -0500

    update ips

--- project/mysql-cluster-master.sh
+++ project/mysql-cluster-master.sh
@@ -5,9 +5,7 @@ sudo apt update
 
 echo "mysql cluster master" >> /var/log/user-data.log
 
-mysql_ubuntu=7.6.6-1ubuntu18.04_amd64.deb
-
-version=mysql-cluster-community-management-server_$mysql_ubuntu
+version=mysql-cluster-community-management-server_7.6.6-1ubuntu18.04_amd64.deb
 
 wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/$version
 sudo dpkg -i $version
@@ -33,31 +31,31 @@ sudo apt -y install libncurses5
 echo "ndb_mgmd" >> /var/log/user-data.log
 sudo ndb_mgmd -f /var/lib/mysql-cluster/config.ini --ndb-nodeid=1 >> /var/log/user-data.log
 
-echo "custom cluster" >> /var/log/user-data.log
-version=mysql-cluster_$mysql_ubuntu-bundle.tar
-wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/$version
-mkdir install
-tar -xvf $version -C install/
-cd install
-sudo apt install libaio1 libmecab2
-echo "dependencies" >> /var/log/user-data.log
-sudo dpkg -i mysql-common_$mysql_ubuntu
-echo "1" >> /var/log/user-data.log
-sudo dpkg -i mysql-cluster-community-client_$mysql_ubuntu
-echo "2" >> /var/log/user-data.log
-sudo dpkg -i mysql-client_$mysql_ubuntu
-echo "3" >> /var/log/user-data.log
-sudo dpkg -i mysql-cluster-community-server_$mysql_ubuntu
-echo "4" >> /var/log/user-data.log
-sudo dpkg -i mysql-server_$mysql_ubuntu
+# echo "custom cluster" >> /var/log/user-data.log
+# version=mysql-cluster_7.6.6-1ubuntu18.04_amd64.deb-bundle.tar
+# wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/$version
+# mkdir install
+# tar -xvf $version -C install/
+# cd install
+# sudo apt install libaio1 libmecab2
+# echo "dependencies" >> /var/log/user-data.log
+# sudo dpkg -i mysql-common_7.6.6-1ubuntu18.04_amd64.deb
+# echo "1" >> /var/log/user-data.log
+# sudo dpkg -i mysql-cluster-community-client_7.6.6-1ubuntu18.04_amd64.deb
+# echo "2" >> /var/log/user-data.log
+# sudo dpkg -i mysql-client_7.6.6-1ubuntu18.04_amd64.deb
+# echo "3" >> /var/log/user-data.log
+# sudo dpkg -i mysql-cluster-community-server_7.6.6-1ubuntu18.04_amd64.deb
+# echo "4" >> /var/log/user-data.log
+# sudo dpkg -i mysql-server_7.6.6-1ubuntu18.04_amd64.deb
 
-echo "restart mysql" >> /var/log/user-data.log
-sudo systemctl restart mysql
+# echo "restart mysql" >> /var/log/user-data.log
+# sudo systemctl restart mysql
 
-echo "download sample db" >> /var/log/user-data.log
-sudo wget https://downloads.mysql.com/docs/sakila-db.tar.gz && sudo tar -xf sakila-db.tar.gz -C /tmp/>> /var/log/user-data.log
+# echo "download sample db" >> /var/log/user-data.log
+# sudo wget https://downloads.mysql.com/docs/sakila-db.tar.gz && sudo tar -xf sakila-db.tar.gz -C /tmp/>> /var/log/user-data.log
 
-sudo mysql -u root -pabcd -e "SOURCE /tmp/sakila-db/sakila-schema.sql;"
-sudo mysql -u root -pabcd -e "SOURCE /tmp/sakila-db/sakila-data.sql;"
-sudo mysql -u root -pabcd -e "create user 'bruno'@'%' identified by 'bruno';"
-sudo mysql -u root -pabcd -e "GRANT ALL ON *.* TO 'bruno'@'%';"
+# sudo mysql -u root -pabcd -e "SOURCE /tmp/sakila-db/sakila-schema.sql;"
+# sudo mysql -u root -pabcd -e "SOURCE /tmp/sakila-db/sakila-data.sql;"
+# sudo mysql -u root -pabcd -e "create user 'bruno'@'%' identified by 'bruno';"
+# sudo mysql -u root -pabcd -e "GRANT ALL ON *.* TO 'bruno'@'%';"
