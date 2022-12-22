af7fab4b8d276fcd4f699d880ca2cfae496b1f46
BrunoC-L <bruno.bcl@sympatico.ca>
2022-12-22 03:40:28 -0500

    update ips

--- project/mysql-cluster-master.sh
+++ project/mysql-cluster-master.sh
@@ -18,11 +18,6 @@ sudo mkdir /var/lib/mysql-cluster
 curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/project/master-config.ini > config.ini
 sudo cp config.ini /var/lib/mysql-cluster/config.ini
 
-echo "my.cnf" >> /var/log/user-data.log
-curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/project/master-my.cnf > my.cnf
-sudo mkdir /etc/mysql
-sudo cp my.cnf /etc/mysql/my.cnf
-
 echo "install" >> /var/log/user-data.log
 sudo apt -y install libtinfo5
 sudo apt -y install libclass-methodmaker-perl
@@ -31,23 +26,24 @@ sudo apt -y install libncurses5
 echo "ndb_mgmd" >> /var/log/user-data.log
 sudo ndb_mgmd -f /var/lib/mysql-cluster/config.ini --ndb-nodeid=1 >> /var/log/user-data.log
 
-# echo "custom cluster" >> /var/log/user-data.log
-# version=mysql-cluster_7.6.6-1ubuntu18.04_amd64.deb-bundle.tar
-# wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/$version
-# mkdir install
-# tar -xvf $version -C install/
-# cd install
-# sudo apt install libaio1 libmecab2
-# echo "dependencies" >> /var/log/user-data.log
-# sudo dpkg -i mysql-common_7.6.6-1ubuntu18.04_amd64.deb
-# echo "1" >> /var/log/user-data.log
-# sudo dpkg -i mysql-cluster-community-client_7.6.6-1ubuntu18.04_amd64.deb
-# echo "2" >> /var/log/user-data.log
-# sudo dpkg -i mysql-client_7.6.6-1ubuntu18.04_amd64.deb
-# echo "3" >> /var/log/user-data.log
+echo "custom cluster" >> /var/log/user-data.log
+version=mysql-cluster_7.6.6-1ubuntu18.04_amd64.deb-bundle.tar
+wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/$version
+mkdir install
+tar -xvf $version -C install/
+cd install
+sudo apt -y install libaio1 libmecab2
+echo "dependencies" >> /var/log/user-data.log
+sudo dpkg -i mysql-common_7.6.6-1ubuntu18.04_amd64.deb
+sudo dpkg -i mysql-cluster-community-client_7.6.6-1ubuntu18.04_amd64.deb
+sudo dpkg -i mysql-client_7.6.6-1ubuntu18.04_amd64.deb
 # sudo dpkg -i mysql-cluster-community-server_7.6.6-1ubuntu18.04_amd64.deb
-# echo "4" >> /var/log/user-data.log
 # sudo dpkg -i mysql-server_7.6.6-1ubuntu18.04_amd64.deb
+
+# echo "my.cnf" >> /var/log/user-data.log
+# curl https://raw.githubusercontent.com/BrunoC-L/LOG8415/main/project/master-my.cnf > my.cnf
+# sudo mkdir /etc/mysql
+# sudo cp my.cnf /etc/mysql/my.cnf
 
 # echo "restart mysql" >> /var/log/user-data.log
 # sudo systemctl restart mysql
