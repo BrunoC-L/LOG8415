07ef9b76579008a21b0c3f1bbbedafdb50ffe155
BrunoC-L <bruno.bcl@sympatico.ca>
2022-12-22 23:32:26 -0500

    update ips

--- project/benchmark.sh
+++ project/benchmark.sh
@@ -1,6 +1,7 @@
 singleIP=44.202.228.30
 sudo mysql --host=$singleIP -ubruno -pbruno -e "use sakila; select * from inventory limit 1;"
 
+
 sudo sysbench oltp_read_write --mysql-user=bruno --mysql-password=bruno --mysql-db=sakila --mysql-host=$singleIP prepare
 sudo sysbench oltp_read_write --mysql-user=bruno --mysql-password=bruno --mysql-db=sakila --mysql-host=$singleIP --time=10 run
 sudo sysbench oltp_read_write --mysql-user=bruno --mysql-password=bruno --mysql-db=sakila --mysql-host=$singleIP --time=10 --threads=4 run
@@ -10,9 +11,12 @@ sudo sysbench oltp_read_write --mysql-user=bruno --mysql-password=bruno --mysql-
 sudo sysbench oltp_read_write --mysql-user=bruno --mysql-password=bruno --mysql-db=sakila --mysql-host=$singleIP cleanup
 
 
+
+
 masterIP=34.226.153.182
 sudo mysql --host=$masterIP -ubruno -pbruno -e "use sakila; select * from inventory limit 1;"
 sudo mysql --host=$masterIP -ubruno -pbruno -e "show engine ndb status \g;"
+
 
 sudo sysbench oltp_read_write --mysql-user=bruno --mysql-password=bruno --mysql-db=sakila --mysql-host=$masterIP prepare
 sudo sysbench oltp_read_write --mysql-user=bruno --mysql-password=bruno --mysql-db=sakila --mysql-host=$masterIP --time=10 run
